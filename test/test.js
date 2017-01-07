b606fbf7789743ce67db5c6f1bcf43671595e7fd
scottbedard <scottbedard90@gmail.com>
2017-01-07 10:11:21 -0700

    Adding some tests

--- test/test.js
+++ test/test.js
@@ -1,38 +1,63 @@
-var MemoryFileSystem = require('memory-fs');
-var path = require('path');
-var webpack = require('webpack');
+'use strict';
 
-var msf = new MemoryFileSystem();
-var loaderPath = 'expose-loader?' + path.resolve(__dirname, '../index.js');
+var expect = require('chai').expect;
+var loader = require('../lib/loader');
 
-function bundle(options, callback) {
-    var compiler = webpack(Object.assign({}, {
-        output: { path: '/', filename: 'test.build.js' },
-        module: { rules: [{ test: /\.jst$/, loader: loaderPath }] },
-    }, options));
+function load(source, options) {
+    let webpackContext = Object.assign({}, { cacheable: () => null }, options);
+    return loader.bind(webpackContext)(source);
+};
 
-    compiler.outputFileSystem = mfs;
-    webpackCompiler.run((err, stats) => {
-        expect(err).to.be.null;
-        if (stats.compilation.errors.length) {
-            state.compilation.errors.forEach(err => console.error(err.message));
-        }
-
-        expect(stats.compilation.errors).to.be.empty;
-        cb(mfs.readFileSync('/test.build.js').toString());
-    });
+function getTemplateFunction(output) {
+    let templateFunction = output.slice(17); // <- remove module export
+    return new Function('return ' + templateFunction)();
 }
 
 describe('underscore-template-strict-loader', () => {
-    it.skip('pre-compiles underscore templates', () => {
-        // pending
+    it('doesn\'t violate strict mode', () => {
+        let output = load('<%= foo %>');
+        expect(output.indexOf('with')).to.equal(-1);
     });
 
-    it.skip('prefixes variables not defined in the template', () => {
-        // pending
+    it('pre-compiles underscore templates', () => {
+        let output = load('<div>hello</div>')
+        let template = getTemplateFunction(output);
+
+        expect(template()).to.equal('<div>hello</div>');
     });
 
-    it.skip('adds the template path in a comment', () => {
-        // pending
+    it('adds the template path in a comment', () => {
+        let output = load('<div>hello</div>', {
+            resource: 'test.jst',
+            query: '?addFilenameComment=true',
+        });
+
+        let template = getTemplateFunction(output);
+
+        expect(template().indexOf('<!--  test.jst  -->')).to.not.equal(-1);
+    });
+
+    it('doesn\'t prefix templates that are defined in the template', () => {
+        let output = load('<% var foo = "bar" %><%= foo %>');
+        let template = getTemplateFunction(output);
+
+        expect(output.indexOf('data.foo')).to.equal(-1);
+        expect(template()).to.equal('bar');
+    });
+
+    it('prefixes variables not defined in the template', () => {
+        let output = load('<%= foo %>');
+        let template = getTemplateFunction(output);
+
+        expect(output.indexOf('data.foo')).to.not.equal(-1);
+        expect(template({ foo: 'bar' })).to.equal('bar');
+    });
+
+    it('correctly prefixes objects in the template', () => {
+        let output = load('<%= foo.bar %>');
+        let template = getTemplateFunction(output);
+
+        expect(output.indexOf('data.foo.bar')).to.not.equal(-1);
+        expect(template({ foo: { bar: 'baz' } })).to.equal('baz');
     });
 });
